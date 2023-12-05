b4b03f381a47aef0ac0c89dc794c824883a9d55f
TobCraft3521 <tobiashackenberg@outlook.de>
2023-12-05 22:09:22 +0100

    categories 1.0

--- src/app/_components/Categories/index.tsx
+++ src/app/_components/Categories/index.tsx
@@ -1,16 +1,20 @@
 import Link from 'next/link'
 import classes from './index.module.scss'
+import { Category } from '../../../payload/payload-types'
+import CategoryCard from './CategoryCard'
 
-const Categories = () => {
+const Categories = ({ categories }: { categories: Category[] }) => {
   return (
     <section className={classes.container}>
       <div className={classes.titleWrapper}>
         <h3>Shop by Categories</h3>
         <Link href="/products">Show All</Link>
-        <div className={classes.list}>
-            
-        </div>
       </div>
+        <div className={classes.list}>
+          {categories.map(category => (
+            <CategoryCard key={category.id} category={category}/>
+          ))}
+        </div>
     </section>
   )
 }
